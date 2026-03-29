5ad1f932319fd274708dc43ae978eb595987e721
mariajic-cloud <maria.iriartecortez@gmail.com>
2026-03-29 18:24:11 -0300

    primer commit tesis

--- Subida_data.py
+++ Subida_data.py
@@ -1,13 +1,17 @@
 import pandas as pd
-import os
+from pathlib import Path
 
 def buscar_y_cargar(nombre_archivo):
-    ruta_final = os.path.join("archive", nombre_archivo)
+    ruta_base = Path(__file__).resolve().parent
+    ruta_final = ruta_base / "archive" / nombre_archivo
+    print("Cargando desde:", ruta_final)
     return pd.read_csv(ruta_final)
 
 ##Probando formula
-data_train=buscar_y_cargar("fraudTrain.csv")
-#print(data_test.head())
+data_train = buscar_y_cargar("fraudTrain.csv")
+print(data_train.head())  # Descomenta esta línea para ver las primeras filas
 
-
+#Cargar también el archivo de test:
+data_test = buscar_y_cargar("fraudTest.csv")
+print(data_test.head())
 
