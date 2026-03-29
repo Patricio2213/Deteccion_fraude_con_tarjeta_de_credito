dc48ed78c23d242bf0f4eef0e195d223efd9583a
Patricio Alvarez <patricio.alvarez@mail.udp.cl>
2026-03-29 17:01:33 -0300

    se agregó un describe y algunos cambios en formula

--- Tesis.py
+++ Tesis.py
@@ -2,8 +2,26 @@
 from Subida_data import buscar_y_cargar
 data_train=buscar_y_cargar("fraudTrain.csv")
 
+#Verificar presencia de nulos y duplicados
+from procesamiento_bases import ver_nulos
+from procesamiento_bases import ver_duplicados
+print("NULOS POR COLUMNA")
+ver_nulos(data_train)
+
+print("\n CANTIDAD DUPLICADOS")
+ver_duplicados(data_train)
+
+print("\n RESUMEN DE DATOS")
+from procesamiento_bases import resumen
+resumen(data_train)
+
+print("\n CANTIDAD DE FRAUDES")
+from procesamiento_bases import balance_clases
+print(balance_clases(data_train)) #0.58% de fraude
+
+
 #Adición de columnas distancia_km, velocidad_kmh
-from procesamiento_bases import haversine
+from procesamiento_bases import haversine, ver_duplicados
 from procesamiento_bases import calcular_velocidad
 data_train["distancia_km"]= haversine(data_train["lat"],data_train["long"],data_train["merch_lat"],data_train["merch_long"])
 #COMPROBACION
@@ -11,6 +29,19 @@ data_train["distancia_km"]= haversine(data_train["lat"],data_train["long"],data_
 
 data_train["velocidad_kmh"]=calcular_velocidad(data_train)
 #COMPROBACION
-print(data_train["velocidad_kmh"].head())
-#GENERA NaN, REPARAR EL CODIGO PARA CUBRIR CUANDO HAY SOLO 1 TRANSACCION
+#print(data_train["velocidad_kmh"])
+#ZCORE MONTO
+from procesamiento_bases import zcore_monto
+data_train["monto_zcore"]=zcore_monto(data_train)
+
+##EDA
+#separar columnas
+from EDA import separar_columnas
+num_columns, cat_columns=separar_columnas(data_train)
+#Histogramas
+from EDA import histogramas
+#var_claves_num=["amt","velocidad_kmh","distancia_km","monto_zcore"]
+#histogramas(data_train,var_claves_num)
+print(data_train["velocidad_kmh"])
+
 
