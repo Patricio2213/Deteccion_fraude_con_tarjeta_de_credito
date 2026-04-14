343e1609bd35af93f1fa70a69949fe24f571ae9b
Patricio Alvarez <patricio.alvarez@mail.udp.cl>
2026-04-14 16:55:35 -0400

    actualización parcial

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ data_train=buscar_y_cargar("fraudTrain.csv")
 # Crear una lista con el nombre de las variables categóricas
 cat_columns = data_train.select_dtypes(include=['object', 'string']).columns
 
-# Crear una lista con el nombre de las variables numéricas
-num_columns = data_train.select_dtypes(include=['number']).columns
+
+# Definir las que NO queremos (basado en tu matriz de correlación)
+cols_a_eliminar = ['Unnamed: 0', 'cc_num', 'unix_time',"is_fraud"]
+
+# Creamos la lista de numéricas excluyendo las de arriba
+num_columns = data_train.select_dtypes(include=['number']).drop(columns=cols_a_eliminar, errors='ignore').columns
 
 
 # ===================================================================
@@ -85,17 +89,17 @@ print("="*60)
 resumen(data_train)
 
 
-graficar_densidad(data_train,num_columns,target="is_fraud")
-for num_var in num_columns:
-    make_boxplot(data_train,num_var)
+#graficar_densidad(data_train,num_columns,target="is_fraud")
+#for num_var in num_columns:
+ #   make_boxplot(data_train,num_var)
 
-for cat_var in cat_columns:
-    make_barplot(data_train,cat_var,top=15) #univariado
+#for cat_var in cat_columns:
+ #   make_barplot(data_train,cat_var,top=15) #univariado
 
-print("\n Matriz de correlación...")
-make_heat_map(data_train,num_columns)#multivariado
+#print("\n Matriz de correlación...")
+#make_heat_map(data_train,num_columns)#multivariado
 
-graficar_temporalidad_fraude(data_train)
+#graficar_temporalidad_fraude(data_train)
 
 #grafico_tasa_por_variable(data_train, 'category')
 #grafico_tasa_por_variable(data_train, 'es_nuevo')# Analizando Riesgo en Comercios Nuevos
@@ -105,44 +109,13 @@ graficar_temporalidad_fraude(data_train)
 
 #grafico_tasa_por_variable(data_train, "gender") #influye el genero en la probabilidad?
 
-#print("\n" + "="*70)
-#print("EXPLORACIÓN PROFUNDA")
-#print("="*70)
+print("\n" + "="*70)
+print("EXPLORACIÓN PROFUNDA")
+print("="*70)
 
 
-#print("\nTabla de estadísticas comparativas")
-#tabla = tabla_estadisticas_fraude(data_train, var_claves_num)
-
-
-
-#stats_vel_original = data_train.groupby('is_fraud')['velocidad'].describe(percentiles=[.25, .5, .75])
-
-#  Cálculo de los componentes del Boxplot
-#stats_vel_original['IQR'] = stats_vel_original['75%'] - stats_vel_original['25%']
-
-# Bigote Superior: Donde estadísticamente empiezan los outliers
-#stats_vel_original['Bigote_Superior'] = stats_vel_original['75%'] + (1.5 * stats_vel_original['IQR'])
-
-# Mostrar la tabla
-#print("="*65)
-#print("TABLA ESTADÍSTICA")
-#print("="*65)
-#print(stats_vel_original.T)
-
-#tabla_a, tabla_b = generar_tablas_tesis(data_train, var_claves_num)
-
-#print("\n" + "="*80)
-#print("TABLA A: ESTADÍSTICAS DESCRIPTIVAS POR GRUPO")
-#print("="*80)
-#print(tabla_a.to_string(index=False))
-
-#print("\n" + "="*80)
-#print("TABLA B: MÉTRICAS DE COMPARACIÓN Y SEPARACIÓN (FRAUDE VS LEGÍTIMO)")
-#print("="*80)
-#print(tabla_b.to_string(index=False))
-
-
-
+print("\nTabla de estadísticas comparativas")
+tabla = tabla_estadisticas_fraude(data_train, num_columns)
 
 #make_scatter_plot(data_train,var_claves_num)#multivariado  #DA PROBLEMAS, LO ASOCIO A LA CANTIDAD DE OBSERVACIONES
 
@@ -228,3 +201,32 @@ print("\nDISTRIBUCIÓN EN PORCENTAJE (%):")
 print(data_train["es_nuevo"].value_counts(normalize=True) * 100)
 print("="*60)
 
+
+
+
+#TABLAS SIN UNSO ACTUALMENTE PQ NO SE HAN AÑADIDO LAS NUEVAS
+#stats_vel_original = data_train.groupby('is_fraud')['velocidad'].describe(percentiles=[.25, .5, .75])
+
+ # Cálculo de los componentes del Boxplot
+#stats_vel_original['IQR'] = stats_vel_original['75%'] - stats_vel_original['25%']
+
+# Bigote Superior: Donde estadísticamente empiezan los outliers
+#stats_vel_original['Bigote_Superior'] = stats_vel_original['75%'] + (1.5 * stats_vel_original['IQR'])
+
+# Mostrar la tabla
+#print("="*65)
+#print("TABLA ESTADÍSTICA")
+#print("="*65)
+#print(stats_vel_original.T)
+
+#tabla_a, tabla_b = generar_tablas_tesis(data_train, var_claves_num)
+
+#print("\n" + "="*80)
+#print("TABLA A: ESTADÍSTICAS DESCRIPTIVAS POR GRUPO")
+#print("="*80)
+#print(tabla_a.to_string(index=False))
+
+#print("\n" + "="*80)
+#print("TABLA B: MÉTRICAS DE COMPARACIÓN Y SEPARACIÓN (FRAUDE VS LEGÍTIMO)")
+#print("="*80)
+#print(tabla_b.to_string(index=False))
